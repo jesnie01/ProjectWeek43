c063df355b86bdef9539d8dcb68c35f384baf7ef
jesnie01 <jesperbnielsen@live.dk>
2023-11-01 09:37:56 +0100

    Added sql connection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hej");
+    public static void main(String[] args) throws SQLException {
+        //Connect to SQL
+        Properties properties = new Properties();
+        properties.setProperty("user", "sa");
+        properties.setProperty("password", "1234");
+        properties.setProperty("encrypt", "false");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=dbWeek43";
+        Connection connection = DriverManager.getConnection(url, properties);
+
+        //Initialize Scanner
+        Scanner in = new Scanner(System.in);
+
+        //Menu
+        System.out.println("Welcome to The House of Consultants");
+        System.out.println("0. For exit");
+        System.out.println("1. Office Worker");
+        System.out.println("2. Consultant");
+
+        //Validate input
+
+
     }
+
 }
