d64db44e5f2e9fdfc5b8834c9f9c48634dd98ae4
jesnie01 <jesperbnielsen@live.dk>
2023-11-01 10:52:48 +0100

    Added methodstructure

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,34 @@ public class Main {
         //Initialize Scanner
         Scanner in = new Scanner(System.in);
 
+        //Initialize sentinel
+        boolean done = false;
         //Menu
-        System.out.println("Welcome to The House of Consultants");
-        System.out.println("0. For exit");
-        System.out.println("1. Office Worker");
-        System.out.println("2. Consultant");
+        do {
+            System.out.println("Welcome to The House of Consultants");
+            System.out.println("0. For exit");
+            System.out.println("1. Office Worker");
+            System.out.println("2. Consultant");
 
-        //Call menues
-        OfficeWorkerMenu();
+            String stringInput = in.next();
+            switch (stringInput) {
+                case "0":
+                    done = true;
+                    break;
+                case "1":
+                    OfficeWorkerMenu();
+                    break;
+                case "2":
+                    ConsultantMenu();
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+
+        }while(!done);
+        //Call menu
+        /*OfficeWorkerMenu();
             ManageCustomerMenu();
                 CreateCustomer();
                 UpdateCustomer();
@@ -39,12 +59,95 @@ public class Main {
                 UpdateConsultant();
                 SearchConsultant();
 
-        CosultantMenu();
+        ConsultantMenu();
             ManageTaskMenu();
                 CreateTask();
                 UpdateTask();
                 SearchTask();
+*/
 
+    }
+
+    private static void SearchTask() {
+
+    }
+
+    private static void UpdateTask() {
+
+    }
+
+    private static void CreateTask() {
+
+    }
+
+    private static void ManageTaskMenu() {
+
+    }
+
+    private static void ConsultantMenu() {
+
+    }
+
+    private static void SearchConsultant() {
+
+    }
+
+    private static void UpdateConsultant() {
+
+    }
+
+    private static void CreateConsultant() {
+
+    }
+
+    private static void ManageConsultantMenu() {
+
+    }
+
+    private static void SendNotification() {
+
+    }
+
+    private static void GetNotification() {
+
+    }
+
+    private static void NotificationSystem() {
+
+    }
+
+    private static void SearchProject() {
+
+    }
+
+    private static void UpdateProject() {
+
+    }
+
+    private static void CreateProject() {
+
+    }
+
+    private static void ManageProjectMenu() {
+
+    }
+
+    private static void SearchCustomer() {
+
+    }
+
+    private static void UpdateCustomer() {
+
+    }
+
+    private static void CreateCustomer() {
+    }
+
+    private static void ManageCustomerMenu() {
+
+    }
+
+    private static void OfficeWorkerMenu() {
 
     }
 
